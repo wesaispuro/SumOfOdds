0c74939d0f7cc724777d36ad2e02f21f95dfdee6
Wesley Aispuro <wesley.aispuro@outlook.com>
2019-02-14 11:53:16 -0800

    20190214

---
+++ SumOfOdds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOdds
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n\tType a number to get sum of odd numbers: "); // User inputs number to get sum of the odd numbers less than input number
+            Console.Write("\t");
+            Console.WriteLine("\t" + SumOfOdds(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+        }
+
+        public static int SumOfOdds(int n)
+        {
+            int sum = 0;
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        sum += i;
+                    }
+                }return sum;
+            }
+        }
+    }
+}
---
+++ SumOfOddsTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumOfOdds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOdds.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CalcOddTest()
+        {
+            Assert.AreEqual(9, Program.CalcOdd(6));
+        }
+    }
+}
